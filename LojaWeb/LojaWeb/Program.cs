890d0831bf15592e18978404478d90f3a7257f18
Vinijs <vini-spider-6@hotmail.com>
2024-05-21 16:22:37 -0300

    Aula 05 - 21/05/2024

--- LojaWeb/LojaWeb/Program.cs
+++ LojaWeb/LojaWeb/Program.cs
@@ -3,104 +3,39 @@ using LojaWeb.Entidades;
 using LojaWeb.Infra;
 using NHibernate;
 using NHibernate.Cfg;
-
-// ======================= AULA 01 - 15/05/2024 ======================
-
-//Configuration cfg = NHibernateHelper.RecuperaConfiguracao();
-
-//ISessionFactory sessionFactory = cfg.BuildSessionFactory();
-
-//ISession session = sessionFactory.OpenSession();
-
-//Usuario usuario = new Usuario();
-
-//usuario.Nome = "Vinicius";
-
-//ITransaction transaction = session.BeginTransaction();
-
-//session.Save(usuario);
-
-//transaction.Commit();
-
-//session.Close();
-// ===================================================================
-
-// ======================= AULA 02 - 16/05/2024  =====================
-
-//ISession session = NHibernateHelper.AbreSession();
-
-//UsuarioDAO usuarioDAO = new UsuarioDAO(session);
-
-//Usuario usuario = new Usuario();
-
-//usuario.Nome = "Natalia";
-
-//usuarioDAO.Adiciona(usuario);
-
-//session.Close();
-// ===================================================================
-
-// ======================= AULA 03 - 17/05/2024  =====================
+using NHibernate.Transform;
 
 //NHibernateHelper.GeraSchema();
 
-//Categoria umaCategoria = new Categoria();
-//umaCategoria.Nome = "Nova Categoria";
-
-//Produto produto = new Produto();
-
-//produto.Nome = "Calça";
-//produto.Preco = 20;
-
-
-
-//produto.Categoria = umaCategoria;
-
-//ISession session = NHibernateHelper.AbreSession();
-
-//ITransaction transaction = session.BeginTransaction();
-
-//session.Save(umaCategoria);
-
-//session.Save(produto);
-
-//transaction.Commit();
-
-//session.Close();
-// ===================================================================
-
-// ======================= AULA 04 - 19/05/2024  =====================
-
-//ISession session = NHibernateHelper.AbreSession();
-
-//ITransaction transaction = session.BeginTransaction();
-
-//Categoria categoria = session.Load<Categoria>(1);
-
-//IList<Produto> produtos = categoria.Produtos;
-
-//Console.WriteLine(categoria.Produtos.Count);
-
-//transaction.Commit();
-// ===================================================================
-
-// ======================= AULA 05 - 20/05/2024  =====================
+// ======================= AULA 05 - 21/05/2024  =====================
 
 ISession session = NHibernateHelper.AbreSession();
 
-String hql = "from Produto p where p.Categoria.Nome = :categoria";
+String hql = "select p.Categoria as Categoria, count(p) as NumeroDePedidos from Produto p group by p.Categoria";
 
 IQuery query = session.CreateQuery(hql);
 
-query.SetParameter("categoria", "Nova Categoria");
+query.SetResultTransformer(Transformers.AliasToBean<ProdutosPorCategoria>());
 
-IList<Produto> produtos = query.List<Produto>();
 
-foreach (Produto produto in produtos)
-{
-    Console.WriteLine(produto.Nome);
-}
+// IList<Object[]> resultados = query.List<object[]>();
+
+IList<ProdutosPorCategoria> relatorio = query.List<ProdutosPorCategoria>();
+
+//foreach (Object[] resultado in resultados)
+//{
+//    ProdutosPorCategoria p = new ProdutosPorCategoria();
+//    p.Categoria = (Categoria)resultado[0];
+//    p.NumeroDePedidos = (long)resultado[1];
+//    relatorio.Add(p);
+//}
 
 session.Close();
 
 Console.Read();
+
+public class ProdutosPorCategoria
+{
+    public Categoria Categoria { get; set; }
+    public long NumeroDePedidos { get; set; }
+}
